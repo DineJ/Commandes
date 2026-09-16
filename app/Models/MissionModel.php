d5eddf763c8aa96893657b623ccadb373dd2de00
DineJ <dinejridi@gmail.com>
2026-09-16 11:39:44 +0200

    Moved function to the correct place

--- app/Models/MissionModel.php
+++ app/Models/MissionModel.php
@@ -10,18 +10,4 @@ class MissionModel extends Model
 	protected $primaryKey = 'id';
 	protected $returnType = 'App\Entities\Mission';
 	protected $allowedFields = ['id_vehicule', 'id_user', 'id_trajet'];
-
-	public function getMotifEnum()
-	{
-
-		// Get ENUM values
-		$query = $this->db->query("SHOW COLUMNS FROM {$this->table} LIKE 'motif'");
-		$row = $query->getRow();
-
-		// Extract values of the ENUM
-		preg_match("/^enum\((.*)\)$/", $row->Type, $matches);
-
-		// Conversion in a PHP array
-		return str_getcsv($matches[1], ',', "'");
-	}
 }
--- app/Models/TrajetModel.php
+++ app/Models/TrajetModel.php
@@ -10,4 +10,18 @@ class TrajetModel extends Model
 	protected $primaryKey = 'id';
 	protected $returnType = 'App\Entities\Trajet';
 	protected $allowedFields = ['id_lieu_depart', 'id_lieu_arrive', 'date_debut', 'date_arrivee', 'motif', 'km_depart', 'km_arrive'];
+
+	public function getMotifEnum()
+	{
+
+		// Get ENUM values
+		$query = $this->db->query("SHOW COLUMNS FROM {$this->table} LIKE 'motif'");
+		$row = $query->getRow();
+
+		// Extract values of the ENUM
+		preg_match("/^enum\((.*)\)$/", $row->Type, $matches);
+
+		// Conversion in a PHP array
+		return str_getcsv($matches[1], ',', "'");
+	}
 }
