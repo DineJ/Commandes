54c3db003775400ccf36866c9e7199dcdeeae47a
DineJ <dinejridi@gmail.com>
2026-09-16 09:43:13 +0200

    Updated scrit. Created 2 variables to make the script easier to use

--- create_crud.sh
+++ create_crud.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+PHPFPM=php-fpm-commandes
+USER=dine
+GROUP=dine
+
 function help()
 {
 	scriptname=$(basename "$0")
+	echo "In case of script errors, just adapt variables"
 	echo "How to use ${scriptname} : ${scriptname} create <entityname> [field1,field2,field3,...]"
 	echo "                            ${scriptname} rm <entityname>"
 }
@@ -20,11 +25,11 @@ case $action in
 	else
 		mkdir -p app/Entities
 
-		docker compose exec php-fpm php spark generate:models $@
-		docker compose exec php-fpm php spark generate:entity "${1}Model"
-		docker compose exec php-fpm php spark generate:controller $@
-		docker compose exec php-fpm php spark generate:views $@
-		docker compose exec php-fpm php spark generate:routes $@
+		docker compose exec $PHPFPM php spark generate:models $@
+		docker compose exec $PHPFPM php spark generate:entity "${1}Model"
+		docker compose exec $PHPFPM php spark generate:controller $@
+		docker compose exec $PHPFPM php spark generate:views $@
+		docker compose exec $PHPFPM php spark generate:routes $@
 		entity=${1^}
 		sudo chmod -f 0664 app/Views/$entity/*.php  app/Entities/${entity}.php app/Models/${entity}Model.php app/Controllers/${entity}Controller.php
 	fi
@@ -41,5 +46,5 @@ case $action in
     ;;
 esac
 
-sudo chown -Rf ubuntu:ubuntu .
+sudo chown -Rf $USER:$GROUP .
 sudo chown -Rf www-data:www-data writable
