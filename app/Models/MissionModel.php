f8c2af6e01a6cd972aa1e26f0beb73dbde61acaf
DineJ <dinejridi@gmail.com>
2026-09-16 09:45:18 +0200

    Mission was changed in the DB

--- app/Models/MissionModel.php
+++ app/Models/MissionModel.php
@@ -9,7 +9,7 @@ class MissionModel extends Model
 	protected $table = 'mission';
 	protected $primaryKey = 'id';
 	protected $returnType = 'App\Entities\Mission';
-	protected $allowedFields = ['id_vehicule', 'id_user', 'id_lieu_depart', 'id_lieu_arrive', 'motif', 'date_depart', 'date_arrivee', 'km_depart', 'km_arrive'];
+	protected $allowedFields = ['id_vehicule', 'id_user', 'id_trajet'];
 
 	public function getMotifEnum()
 	{
