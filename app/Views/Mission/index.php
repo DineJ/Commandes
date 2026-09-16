de497a23851d2c99335fa3b7cb9801ba19175332
DineJ <dinejridi@gmail.com>
2026-09-16 10:15:49 +0200

    Mission index is fixed. Working as before

--- app/Views/Mission/index.php
+++ app/Views/Mission/index.php
@@ -6,7 +6,7 @@
 <!-- Search bar -->
 <form method="get" action="<?= site_url("Mission") ?>" class="mb-3">
 	<div class="input-group">
-		<input type="text" name="q" class="form-control" placeholder="Rechercher par Nom, Prénom, Plaque (XX-XXX-XX), Date (JJ/MM)" value="<?= isset($search) ?  esc($search) : '' ?>">
+		<input type="text" name="q" class="form-control" placeholder="Rechercher par Nom, Prénom, Plaque (XX-XXX-XX), Date de départ (JJ/MM)" value="<?= isset($search) ?  esc($search) : '' ?>">
 		<button type="submit" class="btn btn-primary">Rechercher</button>
 
 		<!-- Reset search bar -->
@@ -25,14 +25,14 @@
 			<tr>
 				<th>Plaque</th>
 				<th>Conducteur</th>
-				<th>Depart</th>
-				<th>Arrive</th>
+				<th>Départ</th>
+				<th>Arrivé</th>
 				<th>Motif</th>
-				<th>Date départ</th>
+				<th>Date début</th>
 				<th>Date arrivée</th>
 				<th>Km départ</th>
 				<th>Km arrivé</th>
-				<th>Actions</th>
+				<th>Action</th>
 			</tr>
 		</thead>
 
@@ -45,7 +45,7 @@
 					<td class="concatenation" data-label="Lieu départ"><?= esc($item->numero_depart) . ' ' . esc($item->adresse_depart) . ' ' . esc($item->nom_lieu_depart) ?></td>
 					<td class="concatenation" data-label="Lieu arrivé"><?= esc($item->numero_arrive) . ' ' . esc($item->adresse_arrivee) . ' ' . esc($item->nom_lieu_arrive) ?></td>
 					<td data-label="Motif"><?= esc($item->motif) ?></td>
-					<td data-label="Date départ"><?= esc(date('d/m/Y H:i', strtotime($item->date_depart))) ?></td>
+					<td data-label="Date départ"><?= esc(date('d/m/Y H:i', strtotime($item->date_debut))) ?></td>
 					<td data-label="Date arrivée"><?= esc(date('d/m/Y H:i', strtotime($item->date_arrivee))) ?></td>
 					<td data-label="Km départ"><?= esc($item->km_depart) ?></td>
 					<td data-label="Km arrivé"><?= esc($item->km_arrive) ?></td>
