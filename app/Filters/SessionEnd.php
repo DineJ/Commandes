4887ed4ebde89595dfa4e254d1cb77a651ae8dd1
DineJ <dinejridi@gmail.com>
2026-09-17 10:22:09 +0200

    Increase session time for dev

--- app/Filters/SessionEnd.php
+++ app/Filters/SessionEnd.php
@@ -22,7 +22,7 @@ class SessionEnd implements FilterInterface
 		$lastActivity = $session->get('last_activity');
 
 		// If inactive for more than 5 minutes, destroy session and redirect
-		if ($lastActivity && (time() - $lastActivity > 300))
+		if ($lastActivity && (time() - $lastActivity > 300000))
 		{
 			$session->destroy();
 			return redirect()->to('/Login');
